3026617fb77e6995695bece79ff15ff01a844b55
Rohan Bhandari <bhandarirohan556@gmail.com>
2023-01-18 18:38:18 -0500

    node_mod

--- intro_express/server.js
+++ intro_express/server.js
@@ -1,4 +1,27 @@
 
 const express = require('express');
+const superHeroes = require('superheroes');
+const superVillain = require('supervillains');
 
-expre
+const app = express();
+
+// server started on port 3000
+app.listen(3000, function(){
+	console.log('Express server listening on port')
+})
+
+app.get('/', (req, res) => {
+	res.send("Welcome to Home page");
+})
+
+app.get('contac', (req, res) => {
+	res.send("");
+})
+
+app.get('/superhero', (req, res) => {
+	res.send("SuperHero: " + superHeroes.random());
+})
+
+app.get('/supervillain', (req, res) => {
+	res.send("SuperVillain: " + superVillain.random());
+})
