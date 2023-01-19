b25e96e0620dfcb3ec7e4db3ce788273bbcca746
Rohan Bhandari <bhandarirohan556@gmail.com>
2023-01-18 20:04:21 -0500

    Update server
    added more get calls

--- intro_express/server.js
+++ intro_express/server.js
@@ -5,17 +5,24 @@ const superVillain = require('supervillains');
 
 const app = express();
 
+// nodemon monitors any changes in the code and 
+// automatically restarts the server
+
 // server started on port 3000
 app.listen(3000, function(){
 	console.log('Express server listening on port')
 })
 
 app.get('/', (req, res) => {
-	res.send("Welcome to Home page");
+	res.send("<h1>This is Home page</h1>");
 })
 
-app.get('contac', (req, res) => {
-	res.send("");
+app.get('/contact', (req, res) => {
+	res.send("Contact at : test@gmail.com");
+})
+
+app.get('/about', (req, res) => {
+	res.send(" <ul><li>Rohan</li><li>Bhandari</li></ul> ")
 })
 
 app.get('/superhero', (req, res) => {
