30915de2bad2cb78fdfefea202af42d8c3dcd1d3
Rohan Bhandari <bhandarirohan556@gmail.com>
2023-01-22 22:38:13 -0500

    update

---
+++ blog/comments/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { randomBytes } from 'crypto';
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+	res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+
+	const commentId = randomBytes(4).toString('hex');
+	const { content } = req.body;
+
+	const comments = commentsByPostId[req.params.id] || [];
+	
+	comments.push({ id: commentId, content});
+
+	commentsByPostId[req.params.id] = comments;
+
+	res.status(201).send(comments);
+
+});
+
+app.listen(4001, () => {
+	console.log('Listening on 4001');
+});
